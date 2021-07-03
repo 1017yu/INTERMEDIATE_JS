4b020b21530eb71cfed8a481da7b417a21b97742
1017yu <1017yu@daum.net>
2021-07-03 10:55:26 +0900

    Update todo.js

--- To-do-list/todo.js
+++ To-do-list/todo.js
@@ -1,7 +1,9 @@
 const toDoList = document.querySelector('#to-do-list');
 
 function addNewTodo(text) {
-	
+  const li = document.createElement('li');  // createElement 메소드를 활용해서 파라미터에 문자열로 li 를 전달
+  li.textContent = text;  // textContent프로퍼티에 text 값을 할당
+	toDoList.append(li);  // li태그를 toDoList의 마지막 자식 요소로 추가
 }
 
 // 테스트 코드
