b7f124011337ab46e86b57c2db946b384541da90
1017yu <1017yu@daum.net>
2021-07-04 15:37:37 +0900

    Update flag.js

--- flag_game/flag.js
+++ flag_game/flag.js
@@ -7,22 +7,21 @@ function reset() {
 
 // 1. flagUp 함수
 function flagUp(e) {
-  if (e.button == 0) {
-    flagBlue.classList.add('up')
+  if (e.button == 0) {  // button 프로퍼티가 0일 때(마우스 왼쪽 버튼을 누를 때)
+    flagBlue.classList.add('up')  // flagBlue에 up 클래스를 추가
   }
-  else if (e.button == 2) {
-    flagWhite.classList.add('up')
+  else if (e.button == 2) {  // button 프로퍼티가 2일 때(마우스 오른쪽 버튼을 누를 때)
+    flagWhite.classList.add('up')  // flagWhite에 up 클래스를 추가
   }
 
   // 500 밀리초 뒤에 reset함수를 실행
   setTimeout(reset, 500);
 }
 
-// 2. 마우스 오른쪽 버튼 클릭시 나타나는 메뉴창을 막아주세요. 
+// 2. 마우스 오른쪽 버튼 클릭시 나타나는 메뉴창 막기
 document.addEventListener('contextmenu', function (event) {
-  event.preventDefault();
-
+  event.preventDefault();  // 이벤트 객체의 preventDefault 메소드를 사용
 });
 
 // 테스트 코드
-document.addEventListener('mousedown', flagUp);
+document.addEventListener('mousedown', flagUp);  // mousedown 이벤트 시 flagUp 함수 핸들
