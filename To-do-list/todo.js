194a4095ea867953b1741bbddc98a0b1fae58375
Kled <1017yu@daum.net>
2021-08-02 13:38:45 +0900

    Update array_ex.html

--- To-do-list/todo.js
+++ To-do-list/todo.js
@@ -1,13 +1,33 @@
 const toDoList = document.querySelector('#to-do-list');
+const items = toDoList.children;
 
+/*
 function addNewTodo(text) {
   const li = document.createElement('li');  // createElement 메소드를 활용해서 파라미터에 문자열로 li 를 전달
   li.textContent = text;  // textContent프로퍼티에 text 값을 할당
 	toDoList.append(li);  // li태그를 toDoList의 마지막 자식 요소로 추가
 }
+*/ 
+
+function updateToDo(event) {
+  event.target.classList.toggle('done');
+  // 이벤트가 발생한 대상(event.target)에 'done'이라는 클래스 속성값을 toggle(.classList.toggle('done')) 함
+}
+c
+
 
 // 테스트 코드
+/*
 addNewTodo('운동하기');
 addNewTodo('자바스크립트 공부하기');
 addNewTodo('고양이 화장실 청소하기');
 addNewTodo('토익 공부하기');
+*/
+// 세 번째 할일에서는 클릭을 해도 이벤트 핸들러가 동직 되지 않도록 removeEventListener 메소드 사용
+items[2].removeEventListener('click', updateToDo);
+
+
+toDoList.children[2].addEventListener('click', function(e) {e.stopPropagation()});
+// toDoList의 2번째 자식요소에 addEventListener 메소드를 통해 'click' 클릭할 때, 
+// stopPropagation으로 버블링을 막아 addEventLister의 상위 요소인 toDoList의 2번째 자식요소에 클릭 동작이 불가능
+// toDoList의 자식요소 = items 
--- index.js
+++ index.js
@@ -225,3 +225,93 @@ list.append(li);  // 이벤트 버블링을 막으면 동작하지 않게됨
 // 이벤트 버블링: 어떤 요소에서 이벤트가 발생하면 해당 요소에 등록된 이벤트 핸들러가 동작하는 것뿐만 아니라 부모 요소로 이벤트가 계속 전파
 // 이벤트 버블링 막기: stopPropagation() 메소드 사용
 // 이벤트 위임: 자식 요소 각각에 이벤트 핸들러를 하나씩 등록할 필요 없이 부모 요소에서 한 번에 자식 요소들에 발생한 이벤트를 관리할 수 있음
+
+
+/** 
+ * [마우스 버튼 이벤트]
+ * 
+ * > MouseEvent.button
+ * 0: 마우스 왼쪽 버튼
+ * 1: 마우스 휠
+ * 2: 마우스 오른쪽 버튼
+ * 
+ * > MouseEvent.type
+ * click: 마우스 왼쪽 버튼을 눌렀을 때
+ * contextmenu: 마우스 오른쪽 버튼을 눌렀을 때
+ * dblclick: 동일한 위치에서 빠르게 두번 click할 때
+ * mousedown: 마우스 버튼을 누른 순간
+ * mouseup: 마우스 버튼을 눌렀다 뗀 순간
+ */
+
+/** 
+  * [마우스 이동 이벤트]
+ * 
+ * > MouseEvent.type
+ * mousemove: 마우스 포인터가 이동할 때
+ * mouseover: 마우스 포인터가 요소 밖에서 안으로 이동할 때
+ * mouseout: 마우스 포인터가 요소 안에서 밖으로 이동할 때 
+ *
+ * > MouseEvent.clientX, clientY
+ * > MouseEvent.pageX, pageY
+ * : 웹 문서 전체 기준 마우스 포인터 위치
+ * 
+ * > MouseEvent.offsetX, offsetY
+ * : 이벤트가 발생한 요소 기준 마우스 포인터 위치
+ */
+ 
+const box1 = document.querySelector('#box1');
+
+function onMouseMove(e) {
+  console.log(`client: (${e.clientX}, ${e.clientY})`);  // 화면에 표시되는 창의 기준
+  console.log(`page: (${e.pageX}, ${e.pageY})`);  // 문서 전체를 기준
+  console.log(`offset: (${e.offsetX}, ${e.offsetY})`);  // 요소 내의 구역을 기준
+  console.log('------------------------------------');
+}
+
+box1.addEventListener('mousemove', onMouseMove);
+
+/** 
+ * [마우스 이동 이벤트]
+ * 
+ * > MouseEvent.type
+ * mousemove: 마우스 포인터가 움직일 때
+ * mouseover: 마우스 포인터가 요소 밖에서 안으로 움직일 때
+ * mouseout: 마우스 포인터가 요소 안에서 밖으로 움직일 때 
+ * 
+ * > MouseEvent.target
+ * : 이벤트가 발생한 요소
+ * 
+ * > MouseEvent.relatedTarget
+ * : 이벤트가 발생하기 직전(또는 직후)에 마우스가 위치해 있던 요소
+ */
+
+ const box2 = document.querySelector('#box2');
+
+ function printEventData(e) {
+   console.log('event:', e.type);
+   console.log('target:', e.target);
+   console.log('relatedTarget:', e.relatedTarget);
+   console.log('------------------------------------');
+   if (e.target.classList.contains('cell')) {
+     e.target.classList.toggle('on');
+   }
+ }
+ 
+ box2.addEventListener('mouseover', printEventData);
+ box2.addEventListener('mouseout', printEventData);
+
+
+ /** 
+ * [키보드 이벤트]
+ * 
+ * > KeyboardEvent.type
+ * keydown: 키보드 버튼을 누른 순간
+ * keypress: 키보드 버튼을 누른 순간
+ * keyup: 키보드 버튼을 눌렀다 뗀 순간
+ * 
+ * > KeyboardEvent.key
+ * : 이벤트가 발생한 버튼의 값
+ * 
+ * > KeyboardEvent.code
+ * : 이벤트가 발생한 버튼의 키보드에서 물리적인 위치
+ */
